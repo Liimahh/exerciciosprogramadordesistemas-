4449ee69857df1f4007d9d4e3753a0791628a5c7
Liimahh <nathally.plima@senacsp.edu.br>
2025-03-19 16:52:08 -0300

    "Calculo Idade"

---
+++ calculodeidade/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculodeidade
+{
+    public partial class FrmCalculodeIdade : Form
+    {
+        public FrmCalculodeIdade()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int anonascimento, anoatual, idade;
+
+            anonascimento = int.Parse(txtAnoNasc.Text);
+
+            anoatual = int.Parse(txtAnoAtual.Text);
+
+            idade = anoatual - anonascimento;
+
+            lblIdade.Text = idade.ToString();
+
+
+        }
+    }
+}
